25a4ca82930359b17b6b754e4ae71cf1255acc66
Rose Maina <rosewairimu27@gmail.com>
2022-09-11 17:32:40 +0300

    Modified models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,18 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  # get "/" do
+  #   { message: "Good luck with your project!" }.to_json
+  # end
+
+  get "/comments" do 
+    comments = Comment.all
+    comments.to_json
   end
 
+  post "/comments" do
+    comments = Comment.create(username: params[:username], comment: params[:comment])
+    comments.to_json
+  end
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,32 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+Comment.create(
+    username: "Rose",
+    comment: "Ugali Recipe: Yummy!! Great job. 
+        An easier method is to mix the maize with cold water in a bowl, to make a paste. 
+        Then add tothe boiling water. After it bubbles slowly add maize. Less lumps less work"
+)
+Comment.create(
+    username: "Finley",
+    comment: "Ugali is not seasoned....the magic happens with the stew and greens.
+        ugali on its own is bland which makes the spicy stew and greens the perfect accompaniments.
+        so you don't have to worry about it been plain, when you are eating the full dish you won't even notice"
+)
+Comment.create(
+    username: "Arya",
+    comment: "Love your Rice recipes, am a big fun of yours, always making our kitchen have that sweet aroma.  
+        Pliz recommend the best non stick cooking pots and where to find them, regards"
+)
+Comment.create(
+    username: "Alice",
+    comment: "i tried this Mashed Potato recipe and was so sweet; less work"
+)
+Comment.create(
+    username: "Faith",
+    comment: "Wow. Am so thrilled to come across your Rice recipe.  
+        Such a vibe and great receipies. Vibing and cooking along!!."
+)
+
 
 puts "✅ Done seeding!"
